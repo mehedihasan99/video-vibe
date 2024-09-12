9a617f9d1b64e7b10f8f0b0807e65ae34b9273ac
mehedihasan99 <hmhasan.mehedi2003@gmail.com>
2024-09-12 11:42:24 +0600

    add icon in header

--- src/components/Header/UserActions.jsx
+++ src/components/Header/UserActions.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
-
+import notificationIcon from '../../assets/icon/notifications.svg'
+import uploadIcon from '../../assets/icon/upload.svg'
+import userIcon from '../../assets/icon/user-pro.svg'
 export default function UserActions() {
   return (
     <div className="flex items-center space-x-2 lg:space-x-4">
@@ -7,11 +9,7 @@ export default function UserActions() {
         aria-label="Upload"
         className="focus:outline-none hover:bg-gray-100 dark:hover:bg-gray-800 p-2 rounded-full transition duration-200 ease-in-out"
       >
-        <img
-          className="w-6 h-6 md:w-8 md:h-8"
-          src="/src/assets/icon/upload.svg"
-          alt="Upload"
-        />
+        <img className="w-6 h-6 md:w-8 md:h-8" src={uploadIcon} alt="Upload" />
       </button>
       <button
         aria-label="Notifications"
@@ -19,13 +17,13 @@ export default function UserActions() {
       >
         <img
           className="w-6 h-6 md:w-8 md:h-8"
-          src="/src/assets/icon/notifications.svg"
+          src={notificationIcon}
           alt="Notifications"
         />
       </button>
       <img
         className="w-8 h-8 md:w-10 md:h-10 rounded-full cursor-pointer"
-        src="/src/assets/icon/user-pro.svg"
+        src={userIcon}
         alt="User Profile"
       />
     </div>
