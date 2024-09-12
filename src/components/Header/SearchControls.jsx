9eea9a001cce424b1f3430ef59135e754c0afe8e
mehedihasan99 <hmhasan.mehedi2003@gmail.com>
2024-09-12 11:48:20 +0600

    add icon in header

--- src/components/Header/SearchControls.jsx
+++ src/components/Header/SearchControls.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
-
+import searchIcon from '../../assets/icon/search.svg'
+import voiceSearchIcon from '../../assets/icon/voice-search-icon.svg'
 export default function SearchControls() {
   const [searchText, setSearchText] = useState('')
 
@@ -21,21 +22,13 @@ export default function SearchControls() {
         className="h-10 bg-gray-200 rounded-r-full px-4 py-2 border border-gray-300 dark:bg-gray-800 dark:hover:bg-gray-700 transition duration-200 ease-in-out"
         aria-label="Search Button"
       >
-        <img
-          className="w-6 h-6"
-          src="/src/assets/icon/search.svg"
-          alt="Search"
-        />
+        <img className="w-6 h-6" src={searchIcon} alt="Search" />
       </button>
       <button
         className="ml-2 p-2 bg-gray-200 h-10 dark:bg-gray-800 rounded-full hidden lg:inline-flex hover:bg-gray-300 dark:hover:bg-gray-700 transition duration-200 ease-in-out"
         aria-label="Voice Search"
       >
-        <img
-          className="w-6 h-6"
-          src="/src/assets/icon/voice-search-icon.svg"
-          alt="Voice Search"
-        />
+        <img className="w-6 h-6" src={voiceSearchIcon} alt="Voice Search" />
       </button>
     </div>
   )
